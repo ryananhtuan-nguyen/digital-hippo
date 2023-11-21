b8f3f78587cd290710eeaa8e60fbd97b3f2d1938
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-22 09:56:57 +1300

    starting nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { Icons } from './Icon'
+import NavItems from './NavItems'
 
 const Navbar = () => {
   return (
@@ -16,6 +17,10 @@ const Navbar = () => {
                 <Link href="/">
                   <Icons.logo className="h-10 w-10" />
                 </Link>
+              </div>
+
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
               </div>
             </div>
           </div>
