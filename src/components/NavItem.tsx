db1de0204330ea3a5d9412bd47f4fdea5afb936a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-22 10:06:15 +1300

    individual item for nav

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React from 'react'
+import { Button } from './ui/button'
+import { PRODUCT_CATEGORIES } from '@/config'
+import { ChevronDown } from 'lucide-react'
+import { cn } from '@/lib/utils'
+
+type Category = (typeof PRODUCT_CATEGORIES)[number]
+
+interface NavItemProps {
+  category: Category
+  handleOpen: () => void
+  isOpen: boolean
+  isAnyOpen: boolean
+}
+
+const NavItem = ({ category, handleOpen, isAnyOpen, isOpen }: NavItemProps) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? 'secondary' : 'ghost'}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn('h-4 w-4 transition-all text-muted-foreground', {
+              '-rotate-180': isOpen,
+            })}
+          />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default NavItem
