4aa31efc6c2afb9ba1367e048033271483e2418a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-22 23:34:59 +1300

    make new trpc route

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,8 @@
-import { publicProcedure, router } from './trpc'
+import { authRouter } from './auth-router'
+import { router } from './trpc'
 
 export const appRouter = router({
-  anyApiRoute: publicProcedure.query(() => {
-    return 'Hello'
-  }),
+  auth: authRouter,
 })
 
 export type AppRouter = typeof appRouter
