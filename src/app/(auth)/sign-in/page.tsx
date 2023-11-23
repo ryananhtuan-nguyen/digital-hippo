b7bb80acf3c64c760d802434dcd61592c0a6cc63
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 23:21:44 +1300

    sign-in page + redirections

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,175 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import { Icons } from '@/components/Icon'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from '@/lib/validators/account-credentials-validator'
+import { trpc } from '@/trpc/client'
+import { ZodError } from 'zod'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+const SignUpPage = () => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const isSeller = searchParams.get('as') === 'seller'
+  const origin = searchParams.get('origin')
+
+  const continueAsSeller = () => {
+    router.push(`?as=seller`)
+  }
+
+  const continueAsBuyer = () => {
+    router.replace(`/sign-in`, undefined)
+  }
+
+  //validating form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  })
+
+  //initialize trpc
+  const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
+    onSuccess: () => {
+      toast.success('Sign in successfully')
+
+      router.refresh()
+
+      if (origin) {
+        router.push(`/${origin}`)
+        return
+      }
+
+      if (isSeller) {
+        router.push('/sell')
+        return
+      }
+
+      router.push('/')
+    },
+    onError: (err) => {
+      if (err.data?.code === 'UNAUTHORIZED') {
+        toast.error('Invalid email or password')
+      }
+    },
+  })
+  //submitting form
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    //send data to server
+    signIn({ email, password })
+  }
+
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Icons.logo className="h-20 w-20" />
+            <h1 className="text-2xl font-bold">
+              Sign-in to your {isSeller ? 'Seller' : ''} account
+            </h1>
+
+            <Link
+              className={buttonVariants({
+                variant: 'link',
+                className: 'gap-1.5',
+              })}
+              href="/sign-up"
+            >
+              Don&apos;t have an account?
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    {...register('email')}
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.email,
+                    })}
+                    placeholder="you@example.com"
+                  />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    {...register('password')}
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.password,
+                    })}
+                    placeholder="Password"
+                    type="password"
+                  />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+
+                <Button>Sign In</Button>
+              </div>
+            </form>
+
+            <div className="relative">
+              <div
+                className="absolute inset-0 flex items-center"
+                aria-hidden="true"
+              >
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
+            </div>
+
+            {isSeller ? (
+              <Button
+                onClick={continueAsBuyer}
+                variant="secondary"
+                disabled={isLoading}
+              >
+                Continue as customer
+              </Button>
+            ) : (
+              <Button
+                variant="secondary"
+                disabled={isLoading}
+                onClick={continueAsSeller}
+              >
+                Continue as seller
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignUpPage
