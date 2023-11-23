e690df2a22b8641f5efb9401b5ccda0cb9391162
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 22:55:18 +1300

    export type for trpc

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { getPayloadClient } from './get-payload'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { nextApp, nextHandler } from './next-utils'
 import { appRouter } from './trpc'
+import { inferAsyncReturnType } from '@trpc/server'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -14,6 +15,8 @@ const createContext = ({
   req,
   res,
 })
+
+export type ExpressContent = inferAsyncReturnType<typeof createContext>
 
 const start = async () => {
   const payload = await getPayloadClient({
