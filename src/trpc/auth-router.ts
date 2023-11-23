ccbd2ba120f0cf25d9dd0a5a1148af2375295dd3
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 21:54:48 +1300

    verifyEmail route

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,8 +2,10 @@ import { AuthCredentialsValidator } from '../lib/validators/account-credentials-
 import { publicProcedure, router } from './trpc'
 import { getPayloadClient } from '../get-payload'
 import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
 
 export const authRouter = router({
+  //================CREATE USER=========================
   createPayloadUser: publicProcedure
     .input(AuthCredentialsValidator)
     .mutation(async ({ input }) => {
@@ -36,5 +38,23 @@ export const authRouter = router({
         },
       })
       return { success: true, sentToEmail: email }
+    }),
+
+  //===================VERIFY EMAIL=======================
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input
+
+      const payload = await getPayloadClient()
+
+      const isVerified = await payload.verifyEmail({
+        collection: 'users',
+        token,
+      })
+      if (!isVerified) throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+      return { success: true }
     }),
 })
