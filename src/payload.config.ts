4743aedabc7d6b0b64873ad05f33f03611550c4a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-24 12:59:07 +1300

    new types generated

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,8 @@ import { Users } from './collection/Users'
 import dotenv from 'dotenv'
 import { Products } from './collection/Products/Products'
 import { Media } from './collection/Media'
+import { ProductFiles } from './collection/ProductFile'
+import { Orders } from './collection/Orders'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -14,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users, Products, Media],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: '/sell',
   },
