8e7e94e368251f4bfb9f76d459a634c3f9e70ea3
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 23:37:19 +1300

    logout route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = async () => {
                   )}
 
                   {user ? (
-                    <UserAccountNav />
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
