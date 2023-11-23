7928e0f079d78f223647f6e5844d3dbed173dd18
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 21:58:38 +1300

    added onboard email to mock token

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,7 +46,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'watermelon.nta@gmail.com',
+        fromAddress: 'onboarding@resend.dev',
         fromName: 'Digital Hippo',
       },
       secret: process.env.PAYLOAD_SECRET,
