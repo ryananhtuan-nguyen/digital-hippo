6b3cd6f7db9145ce28f1918a8af34936719251be
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 22:38:58 +1300

    redirect and toast error/success

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -15,8 +15,11 @@ import {
   TAuthCredentialsValidator,
 } from '@/lib/validators/account-credentials-validator'
 import { trpc } from '@/trpc/client'
+import { ZodError } from 'zod'
+import { useRouter } from 'next/navigation'
 
 const SignUpPage = () => {
+  const router = useRouter()
   //validating form
   const {
     register,
@@ -31,7 +34,22 @@ const SignUpPage = () => {
     onError: (err) => {
       if (err.data?.code === 'CONFLICT') {
         toast.error('This email is already in use. Sign in instead?')
+
+        return
       }
+
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message)
+
+        return
+      }
+
+      toast.error('Something went wrong. Please try again')
+    },
+
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to ${sentToEmail}`)
+      router.push('/verify-email?to' + sentToEmail)
     },
   })
   //submitting form
@@ -73,6 +91,11 @@ const SignUpPage = () => {
                     })}
                     placeholder="you@example.com"
                   />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
                 </div>
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
@@ -84,6 +107,11 @@ const SignUpPage = () => {
                     placeholder="Password"
                     type="password"
                   />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
+                  )}
                 </div>
 
                 <Button>Sign Up</Button>
