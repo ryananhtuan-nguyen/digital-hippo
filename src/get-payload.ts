470706e329113589078247c0245b66b021bd2b28
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 22:23:17 +1300

    sample email

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,7 +46,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'onboarding@resend.dev',
+        fromAddress: 'hello@joshtriedcoding.com',
         fromName: 'Digital Hippo',
       },
       secret: process.env.PAYLOAD_SECRET,
