cc774e6442468239e63f4a0432be8e0ab99b1d91
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 22:54:51 +1300

    LOGIN ROUTE

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,7 @@
+import { ExpressContent } from '@/server'
 import { initTRPC } from '@trpc/server'
 
-const t = initTRPC.context().create()
+const t = initTRPC.context<ExpressContent>().create()
 export const router = t.router
 
 //public
