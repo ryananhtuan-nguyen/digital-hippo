ad5913e0247606fdcd65ff7c1ad56c4b8fe3093a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 16:00:19 +1300

    added image url

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 interface ImageSliderProps {
   urls: string[]
