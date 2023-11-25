b8ac171503eb63ba78f62d5d05fc722b35dc8235
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 16:22:22 +1300

    added button to swipe left and right

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,14 +1,38 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import type SwiperType from 'swiper'
+import { Pagination } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/pagination'
+import { cn } from '@/lib/utils'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 interface ImageSliderProps {
   urls: string[]
 }
 
 const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null)
+
+  const [activeIndex, setActiveIndex] = useState<number>(0)
+
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  })
+
+  useEffect(() => {
+    swiper?.on('slideChange', ({ activeIndex }) => {
+      setActiveIndex(activeIndex)
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      })
+    })
+  }, [swiper, urls])
+
   const activeStyles =
     'active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300'
 
@@ -16,11 +40,50 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
-        <button></button>
-        <button></button>
+        {/* Swipe right button */}
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            swiper?.slideNext()
+          }}
+          aria-label="next image"
+          className={cn(activeStyles, 'right-3 transition', {
+            [inactiveStyles]: slideConfig.isEnd,
+            'hover:bg-primary-300 text-primary-800 opacity-100':
+              !slideConfig.isEnd,
+          })}
+        >
+          <ChevronRight className="h-4 w-4 text-zinc-700" />
+        </button>
+        {/* swipe left button */}
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            swiper?.slidePrev()
+          }}
+          aria-label="previous image"
+          className={cn(activeStyles, 'left-3 transition', {
+            [inactiveStyles]: slideConfig.isBeginning,
+            'hover:bg-primary-300 text-primary-800 opacity-100':
+              !slideConfig.isBeginning,
+          })}
+        >
+          <ChevronLeft className="h-4 w-4 text-zinc-700" />
+        </button>
       </div>
 
-      <Swiper className="h-full w-full">
+      <Swiper
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`
+          },
+        }}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        spaceBetween={50}
+        slidesPerView={1}
+        modules={[Pagination]}
+        className="h-full w-full"
+      >
         {urls.map((url, i) => (
           <SwiperSlide key={i} className="-z-10 relative h-full w-full">
             <Image
