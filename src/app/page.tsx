79b2231f5a985e43d6f021fbf7788132cdad5242
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 14:33:53 +1300

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const perks = [
   {
-    name: 'Instant Deliver',
+    name: 'Instant Delivery',
     Icon: ArrowDownToLine,
     description:
       'Get your assets delivered to your email in seconds and download them right away.',
