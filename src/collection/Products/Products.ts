8bd320ee11a26dfd78e86d6ee7442f3bd16dd3a2
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 20:11:56 +1300

    added hook to create product on stripe

--- src/collection/Products/Products.ts
+++ src/collection/Products/Products.ts
@@ -1,5 +1,17 @@
+import { BeforeChangeHook } from 'payload/dist/collections/config/types'
 import { PRODUCT_CATEGORIES } from '../../config'
 import { CollectionConfig } from 'payload/types'
+import { Product } from '../../payload-types'
+import { stripe } from '../../lib/stripe'
+
+const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
+  const user = req.user
+
+  return {
+    ...data,
+    user: user.id,
+  }
+}
 
 export const Products: CollectionConfig = {
   slug: 'products',
@@ -7,6 +19,58 @@ export const Products: CollectionConfig = {
     useAsTitle: 'name',
   },
   access: {},
+  hooks: {
+    beforeChange: [
+      addUser,
+      //hook to create product on stripe
+      async (args) => {
+        //new product
+        if (args.operation === 'create') {
+          //get data from our database
+          const data = args.data as Product
+
+          //create product on stripe with an api call
+          const createdProduct = await stripe.products.create({
+            name: data.name,
+            default_price_data: {
+              currency: 'USD',
+              unit_amount: Math.round(data.price * 100),
+            },
+          })
+
+          //get the product from stripe with stripe price id,
+          //update the product in local database
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
+          }
+
+          return updated
+          //else existing product just updating price
+        } else if (args.operation === 'update') {
+          //get data from our database
+          const data = args.data as Product
+
+          //create product on stripe with an api call
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!,
+          })
+
+          //get the product from stripe with stripe price id,
+          //update the product in local database
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          }
+
+          return updated
+        }
+      },
+    ],
+  },
   fields: [
     {
       name: 'user',
