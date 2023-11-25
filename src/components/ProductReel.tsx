1c4eb6e52837e5449c6045362e29f93fde7e8f6d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 15:45:09 +1300

    added image slider

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -3,6 +3,7 @@ import { TQueryValidator } from '@/lib/validators/query-validator'
 import { Product } from '@/payload-types'
 import { trpc } from '@/trpc/client'
 import Link from 'next/link'
+import ProductListing from './ProductListing'
 
 interface ProductReelProps {
   title: string
@@ -64,7 +65,7 @@ const ProductReel = (props: ProductReelProps) => {
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {map.map((product, i) => (
-              <ProductListing />
+              <ProductListing product={product} index={i} key={product?.id} />
             ))}
           </div>
         </div>
