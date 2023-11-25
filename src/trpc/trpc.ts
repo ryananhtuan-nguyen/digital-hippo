b9e0fa7af707d301d80e5f594e1849bc71df7230
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 19:40:30 +1300

    added middleware for private procedure

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,8 +1,30 @@
+import { User } from '@/payload-types'
 import { ExpressContent } from '@/server'
-import { initTRPC } from '@trpc/server'
+import { TRPCError, initTRPC } from '@trpc/server'
+import { PayloadRequest } from 'payload/types'
 
 const t = initTRPC.context<ExpressContent>().create()
 export const router = t.router
 
+//middleware for privateProcedure
+const middleware = t.middleware
+const isAuth = middleware(async ({ ctx, next }) => {
+  const req = ctx.req as PayloadRequest
+
+  const { user } = req as { user: User | null }
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+
+  return next({
+    ctx: {
+      user,
+    },
+  })
+})
+
 //public
 export const publicProcedure = t.procedure
+//private
+export const privateProcedure = t.procedure.use(isAuth)
