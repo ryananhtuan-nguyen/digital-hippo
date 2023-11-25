248edb6957e3c61a8dac01f140a426100613f743
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 18:46:23 +1300

    added product details

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,10 +1,18 @@
+import { PRODUCT_CATEGORIES } from '@/config'
+import { useCart } from '@/hooks/use-cart'
+import { formatPrice } from '@/lib/utils'
 import { Product } from '@/payload-types'
-import { ImageIcon } from 'lucide-react'
+import { ImageIcon, X } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
 const CartItem = ({ product }: { product: Product }) => {
   const { image } = product.images[0]
+  const { removeItem } = useCart()
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label
 
   return (
     <div className="space-y-3 py-2">
@@ -27,6 +35,32 @@ const CartItem = ({ product }: { product: Product }) => {
               </div>
             )}
           </div>
+
+          <div className="flex flex-col self-start">
+            <span className="line-clamp-1 text-sm font-medium mb-1">
+              {product.name}
+            </span>
+
+            <span className="line-clamp-1 text-xs capitalize text-muted-foreground">
+              {label}
+            </span>
+
+            <div className="mt-4 text-xs text-muted-foreground">
+              <button
+                onClick={() => removeItem(product.id)}
+                className="flex items-center gap-0.5"
+              >
+                <X className="w-3 h-4" />
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col space-y-1 font-medium">
+          <span className="ml-auto line-clamp-1 text-sm">
+            {formatPrice(product.price)}
+          </span>
         </div>
       </div>
     </div>
