713d88126980bbfee634ad4e991effc343c30132
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 20:13:22 +1300

    done creating stripe session

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 import { privateProcedure, router } from './trpc'
 import { TRPCError } from '@trpc/server'
 import { getPayloadClient } from '../get-payload'
+import { stripe } from '../lib/stripe'
+import type Stripe from 'stripe'
 
 export const paymentRouter = router({
   createSession: privateProcedure
@@ -25,7 +27,54 @@ export const paymentRouter = router({
           },
         },
       })
+      //getting valid products with price id
+      const filteredProducts = products.filter((prod) => Boolean(prod.priceId))
 
-      //
+      const order = await payload.create({
+        collection: 'orders',
+        data: {
+          _isPaid: false,
+          products: filteredProducts,
+          user: user.id,
+        },
+      })
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+      filteredProducts.forEach((product) => {
+        line_items.push({
+          price: product.priceId!,
+          quantity: 1,
+        })
+      })
+
+      line_items.push({
+        price: 'price_1OGFpTDMEieWOuYjtUm3PU0I',
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false,
+        },
+      })
+
+      //creating session
+      try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ['card'],
+          mode: 'payment',
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+          },
+          line_items,
+        })
+
+        return { url: stripeSession.url }
+      } catch (error) {
+        console.log(error)
+
+        return { url: null }
+      }
     }),
 })
