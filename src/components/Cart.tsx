700475ed11d88d38edb7fdc19d2234712ff1a0bb
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-25 17:22:18 +1300

    changed count to dynamic values

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -41,12 +41,12 @@ const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {itemCount}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
