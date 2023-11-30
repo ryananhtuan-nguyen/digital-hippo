c7361cc760d7c0f0ab500a16e3ecf8392240ed4f
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-30 21:43:32 +1300

    fixed typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer = () => {
                   </h3>
                   <p className="mt-2 text-sm text-muted-foreground">
                     If you&apos;d like to sell high-quality digital products,
-                    you can do so in minutes. {' ]'}
+                    you can do so in minutes.
                     <Link
                       href="/sign-in?as=seller"
                       className="whitespace-nowrap font-medium text-black hover:text-zinc-900"
